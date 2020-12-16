b742606fef3a8ceb1091b76c5cd9d4b055eaa8e0
pkacp <p.kacprzak.96@gmail.com>
2020-12-16 01:45:10 +0100

    Work on symbols and strings keys

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,14 +1,9 @@
 class Activity
   def initialize(params)
-    # valid params
-    @participants = params[:participants]
+    @participants = params[:participants] || params['participants']
   end
 
   def multiple_participants?
     @participants > 1
   end
-  #
-  # def valid(params)
-  #   raise TypeError unless params[:participants].is_a? Integer
-  # end
 end
